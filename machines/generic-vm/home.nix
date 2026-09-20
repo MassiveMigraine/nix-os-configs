9e44dbeb75b93680ff891cf982e86316e397e3b7
MassiveMigraine <MassiveMigraine@gmail.com>
2026-09-20 09:56:15 -0500

    generic-vm fixes

---
+++ machines/generic-vm/home.nix
@@ -0,0 +1,11 @@
+{  config, ... }:
+let
+  machine_files = "${config.home.homeDirectory}/.nix-config/machines/generic-vm";
+in
+{
+  # folders
+  #home.file = {
+  #  ".config/autorandr".source = config.lib.file.mkOutOfStoreSymlink "${machine_files}/autorandr";
+  #};
+
+}
